2e82b5403e86b2ba9fab7f4a6f42a27a9478496c
hemAshr1 <hemashrim25@gmail.com>
2026-04-09 14:44:36 +0530

    UC1: Application Entry & Welcome Message

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+/**
+ * UseCase1HotelBookingApp
+ *
+ * This class represents the entry point of the Hotel Booking Management System.
+ * It demonstrates how a Java application starts execution and prints output to the console.
+ *
+ * @author Hemashri
+ * @version 1.0
+ */
+public class UseCase1HotelBookingApp {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    /**
+     * Main method - entry point of the application
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // Printing welcome message
+        System.out.println("=======================================");
+        System.out.println("   Welcome to Book My Stay App");
+        System.out.println("   Hotel Booking System v1.0");
+        System.out.println("=======================================");
+
+        // Application end message
+        System.out.println("Application Started Successfully!");
+        System.out.println("Thank you for using the system.");
     }
 }
